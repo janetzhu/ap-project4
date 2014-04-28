82ffa5f729d445d52a036ed71d5e734db71fe0f8
michaelalexander2015 <mng.excel@gmail.com>
2014-04-28 13:21:45 -0400

    Image Reformat
    Reformatted images for background and instruction screens. Still really
    not sure if this is the look we’re going for. Let me know what you
    think.

--- AP Project 4/src/object/DisplayPanel.java
+++ AP Project 4/src/object/DisplayPanel.java
@@ -122,11 +122,11 @@ public class DisplayPanel extends JPanel {
         }
         else if (GAME_SCREENS[panelType] == "Background") {
 			g2.drawImage(what_is_hiv,0,6,this);
-			g2.drawImage(HIV_attacks, 0, 200, this);
+			g2.drawImage(HIV_attacks, 25, 375, this);
         }
         else if (GAME_SCREENS[panelType] == "Instructions") {
 			g2.drawImage(instructions_img,0,6,this);
-			g2.drawImage(HIV_invasion, 0, 200, this);
+			g2.drawImage(HIV_invasion, 25, 350, this);
         }   
        
 	}
